9b1a98682e536cd3ef738ef3df46fb305145b62a
senortrashcan <79335644+senortrashcan@users.noreply.github.com>
2022-02-22 16:43:31 -0500

    Add files via upload

---
+++ build/precache-manifest.38b18ef35ecce4ba224899fe2d43a1ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d736ac0173aea4790602",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.5ccbd905.chunk.css"
+  },
+  {
+    "revision": "d736ac0173aea4790602",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.d736ac01.chunk.js"
+  },
+  {
+    "revision": "d9202356cf8fe8d76428",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.d9202356.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "e02aa5a4f83736a0f65c4952a28b099c",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
